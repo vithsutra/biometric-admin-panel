2637fedcc1528213f2d72bee2aa97d27235355fa
Anikethanshetty <shettyanikethand@gmail.com>
2025-07-04 16:08:15 +0530

    added image

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,25 @@
-import { LoginForm } from "@/components/login-form"
-import Image from "next/image"
+import { LoginForm } from "@/components/login-form";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-       <div className="bg-muted relative hidden lg:block">
+      <div className=" relative hidden lg:block h-[700px] w-[500px] mt-[100px] ml-[200px]">
         <Image
-          src="/placeholder.svg"
+          src="/bio.svg"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          fill
+          className="object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
-      <div className=" w-full flex flex-col gap-4 p-6 md:p-10">
 
+      <div className="w-full flex flex-col gap-4 p-6 md:p-10">
         <div className="w-full flex flex-1 items-center justify-center">
           <div className="w-[50%]">
             <LoginForm />
           </div>
         </div>
       </div>
-     
     </div>
-  )
+  );
 }
